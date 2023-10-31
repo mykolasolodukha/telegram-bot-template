da8949169ebba3080292d2d36b03db45d63406bd
Vlad <76848642+Salz0@users.noreply.github.com>
2023-10-31 23:18:17 +0200

    Update settings.py
    We indeed don't need to check it in Pydantic if they are nit required

--- {{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/settings.py
@@ -5,24 +5,6 @@ class Settings(BaseSettings):
     """Settings for the Telegram bot."""
 
     TELEGRAM_BOT_TOKEN: str
-    DATABASE_URL: Optional[str] = None
-    REDIS_URL: Optional[str] = None
-
-    @validator("DATABASE_URL", "REDIS_URL", pre=True, always=True)
-    def check_url_scheme(cls, value: Optional[str]):
-        if value is None:
-            return value
-
-        # Now, validate against the DSN types
-        try:
-            if "postgres" in value:
-                return PostgresDsn.validate(value)
-            # elif "redis" in value:
-            #     return RedisDsn.validate(value)
-        except ValidationError:
-            raise ValueError(f"Invalid URL provided for {value}")
-
-        return value
 
     class Config:
         """Configuration for the settings."""
